e961967d0d36af0e95cbab881e307b4d88f63e43
Jed Fox <git@jedfox.com>
2020-09-08 18:15:45 -0400

    Initial Commit

---
+++ Days of March/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Days of March
+//
+//  Created by Jed Fox on 9/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Days of March/Days_of_MarchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Days_of_MarchApp.swift
+//  Days of March
+//
+//  Created by Jed Fox on 9/8/20.
+//
+
+import SwiftUI
+
+@main
+struct Days_of_MarchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
